6f99937f3fc33c9af01897504ed8fc370cd0bd09
irvyncornejo <irvyncornejo@gmail.com>
2024-06-10 23:28:29 -0600

    Add home

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,6 +1,6 @@
 
 from rest_framework import serializers
-from .models import People, Film, Planet
+from .models import People, Film
 
 class UserSerializer(serializers.Serializer):
     email=serializers.EmailField(required=True)
@@ -19,17 +19,31 @@ class FilmsSerializer(serializers.ModelSerializer):
 class PeopleSerializer(serializers.ModelSerializer):
     films = FilmsSerializer(many=True, read_only=True)
     homeworld=serializers.CharField(source='homeworld.name', read_only=True)
+    img=serializers.SerializerMethodField('get_img')
+    height=serializers.SerializerMethodField('get_height')
+    mass=serializers.SerializerMethodField('get_mass')
 
     class Meta:
         model=People
         fields=[
+            'id',
             'name',
             'height',
             'mass',
             'hair_color',
             'gender',
             'dob',
-            'img',
             'homeworld',
-            'films'
+            'films',
+            'img'
         ]
+    def get_img(self, obj):
+        if not obj.img:
+            return 'https://blog.spoongraphics.co.uk/wp-content/uploads/2012/star-wars/29.jpg'
+        return obj.img
+
+    def get_height(self, obj):
+        return '{} M'.format(obj.height)
+    
+    def get_mass(self, obj):
+        return '{} Kg'.format(obj.mass)
